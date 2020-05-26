77735e68c53b6c0363aa357127acc0fe75e1bbf9
Fox Chen <fmacgt@gmail.com>
2020-05-26 17:21:31 -0400

    added a simple main loop to automatically update timers

--- scripts/game.js
+++ scripts/game.js
@@ -2,3 +2,19 @@
 const timerController = new TimerController();
 const moneyController = new MoneyController();
 const businessController = new BusinessController(timerController, moneyController);
+
+/////////////////////////////////////////////////////////////////////////////////////
+
+let prevTimestamp = 0;
+
+function mainLoop(timestamp)
+{
+    let dt = prevTimestamp == 0 ? 0 : timestamp - prevTimestamp;
+    prevTimestamp = timestamp;
+
+    timerController.updateTimers(dt / 0.001);
+
+    requestAnimationFrame(mainLoop);
+}
+
+mainLoop(0);
